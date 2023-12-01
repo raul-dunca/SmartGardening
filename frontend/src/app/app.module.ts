5596a356a054286f01c18c237d2d96ce59ffa1fd
Doszlop-Mark <doszlopmark@yahoo.com>
2023-12-01 12:51:30 +0200

    structure

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { MapComponent } from './components/map/map.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/components/map/map.component.ts
@@ -0,0 +1,9 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.scss']
+})
+export class MapComponent{ 
+  
+}
